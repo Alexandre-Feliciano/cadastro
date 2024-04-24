c642b8868f587909579aec4c6271d8126083741d
unknown <Xande>
2024-04-23 21:18:05 -0300

    Tela de cadastro e  funcionalidades adicionandas no Javascript feitas

--- cadastro.js
+++ cadastro.js
@@ -1,101 +1,104 @@
 let nome = document.querySelector('#nome');
 let LabelNome = document.querySelector('#LabelNome');
-let validNome = false;
+var validNome = false;
 
 let usuario = document.querySelector('#usuario');
-let validUsuario= false;
-let LabelUsuario = document.querySelector('#LabelUsuario');
+var LabelUsuario = document.querySelector('#LabelUsuario');
+var validUsuario= false;
 
 let telefone = document.querySelector('#telefone');
-let LabelTelefone = document.querySelector('#LabelTelefone');
-let validTelefone = false;
+var LabelTelefone = document.querySelector('#LabelTelefone');
+var validTelefone = false;
 
 let senha = document.querySelector('#senha');
-let LabelSenha = document.querySelector('#LabelSenha');
-let validSenha = false;
+var LabelSenha = document.querySelector('#LabelSenha');
+var validSenha = false;
 
 let ConfirmarSenha = document.querySelector('#ConfirmarSenha');
-let LabelConfirmarSenha = document.querySelector('#LabelConfirmarSenha');
-let validConfirmarSenha = false;
+var LabelConfirmarSenha = document.querySelector('#LabelConfirmarSenha');
+var validConfirmarSenha = false;
+
+let msgError = document.querySelector('#msgError');
+let msgSucess = document.querySelector('#msgSucess');
 
 
 nome.addEventListener('keyup', ()=>{
     
     if(nome.value.length <= 3){
         LabelNome.setAttribute('style', 'color: red')
-        LabelNome.innerHTML('Nome *Insira no minimo 3 caracteres ')
+        LabelNome.innerHTML = 'Nome *Insira no minimo 3 caracteres '
         nome.setAttribute('style', 'border-color:red; ')
-        let validNome = false;
+        validNome = false;
         
     }else{
         LabelNome.setAttribute('style', 'color: green;')
-        LabelNome.innerHTML('Nome')
-        nome.setAttribute('style', 'border-color:red; ')        
-        let validNome = true;
+        LabelNome.innerHTML = 'Nome'
+        nome.setAttribute('style', 'border-color: green; ')        
+        validNome = true;
     }
 })
 
 usuario.addEventListener('keyup' , () =>{
     
     if(usuario.value.length <= 4){
-        LabelUsuario.setAttribute('style', 'color: red')
-        LabelUsuario.innerHTML('Nome *Insira no minimo 5 caracteres ')
-        LabelUsuario.setAttribute('style', 'border-color:red; ')
-        let validUsuario = false;
+        LabelUsuario.setAttribute('style', 'color: red;')
+        LabelUsuario.innerHTML = 'Usuário *Insira no minimo 5 caracteres '
+        usuario.setAttribute('style', 'border-color:red; ')
+        validUsuario = false;
         
     }else{
         LabelUsuario.setAttribute('style', 'color: green;')
-        LabelUsuario.innerHTML('Usuário')
-        usuario.setAttribute('style', 'border-color:red; ')        
-        let validUsuario = true;
+        LabelUsuario.innerHTML = 'Usuário'
+        usuario.setAttribute('style', 'border-color:green; ')        
+        validUsuario = true;
     }
 })
 
 telefone.addEventListener('keyup' , () =>{
     
-    if(telefone.value.length == 11 ){
-        LabelTelefone.setAttribute('style', 'color: red')
-        LabelTelefone.innerHTML('Nome *Insira 11 números')
-        LabelTelefone.setAttribute('style', 'border-color:red; ')
-        let validTelefone = false;
+    if(telefone.value.length <= 10 ){
+        LabelTelefone.setAttribute('style', 'color: red;')
+        LabelTelefone.innerHTML = 'Telefone inválido!'
+        telefone.setAttribute('style', 'border-color:red; ')
+        validTelefone = false;
         
     }else{
         LabelTelefone.setAttribute('style', 'color: green;')
-        LabelTelefone.innerHTML('Telefone')
-        telefone.setAttribute('style', 'border-color:red; ')        
-        let validTelefone = true;
+        LabelTelefone.innerHTML = 'Telefone'
+        telefone.setAttribute('style', 'border-color: green; ')        
+        validTelefone = true;
     }
 })
 
 senha.addEventListener('keyup' , () =>{
     
-    if(senha.value.length <= 5 ){
+    if(senha.value.length <= 4 ){
         LabelSenha.setAttribute('style', 'color: red')
-        LabelSenha.innerHTML('Nome *Insira 11 números')
-        LabelSenha.setAttribute('style', 'border-color:red; ')
-        let validSenha = false;
+        LabelSenha.innerHTML = 'A senha deve conter no mínimo 5 números '
+        senha.setAttribute('style', 'border-color:red; ')
+        validSenha = false;
         
     }else{
         LabelSenha.setAttribute('style', 'color: green;')
-        LabelSenha.innerHTML('Senha')
-        senha.setAttribute('style', 'border-color:red; ')        
-        let validSenha = true;
+        LabelSenha.innerHTML = 'Senha'
+        senha.setAttribute('style', 'border-color: green; ')        
+        validSenha = true;
     }
 })
 
-Confirmarsenha.addEventListener('keyup' , () =>{
+ConfirmarSenha.addEventListener('keyup' , () =>{
     
-    if(senha.value == ConfirmarSenha.value ){
-        LabelConfirmarSenha.setAttribute('style', 'color: red')
-        LabelConfirmarSenha.innerHTML('Confirmar senha *As senhas não conferem')
-        LabelConfirmarSenha.setAttribute('style', 'border-color:red; ')
-        let validConfirmarSenha = false;
+    if(senha.value != ConfirmarSenha.value ){
+        LabelConfirmarSenha.setAttribute('style', 'color: red;')
+        LabelConfirmarSenha.innerHTML = 'Confirmar senha *As senhas não conferem'
+        ConfirmarSenha.setAttribute('style', 'border-color:red; ')
+        validConfirmarSenha = false;
         
     }else{
         LabelConfirmarSenha.setAttribute('style', 'color: green;')
-        LabelConfirmarSenha.innerHTML('Confirmar senha')
-        Confirmarsenha.setAttribute('style', 'border-color:red; ')        
-        let validConfirmarSenha = true;
+        LabelConfirmarSenha.innerHTML = 'Confirmar senha'
+        ConfirmarSenha.setAttribute('style', 'border-color: green; ')        
+        validConfirmarSenha = true;
     }
 })
 
@@ -103,10 +106,37 @@ Confirmarsenha.addEventListener('keyup' , () =>{
 
 function cadastrar(){
     if(validNome && validUsuario && validTelefone && validSenha && validConfirmarSenha){
-        alert('Aeeee')
+        
+        let Lista_Users = JSON.parse(localStorage.getItem('Lista_Users') || '[]')
+        
+        Lista_Users.push(
+            
+            {
+                usuario: usuario.value,
+                senha: senha.value             
+            }
+        )
+        
+        localStorage.setItem('Lista_Users', JSON.stringify(Lista_Users))
+        
+        setTimeout(() =>{
+        
+            window.location.href ='https://alexandre-feliciano.github.io/Login/'
+        
+        }, 3000)
 
+        msgSucess.setAttribute('style', 'display: block')
+        msgSucess.innerHTML = ' <strong>Cadastrando usuário...</strong>'
+
+        msgError.setAttribute('style', 'display:none')
+        msgError.innerHTML = ''
     }else{
-        alert('Preencha os campos!')
+        
+        msgError.setAttribute('style', 'display: block')
+        msgError.innerHTML = ' <strong>Preencha todos os campos!</strong>'
+        
+        msgSucess.setAttribute('style', 'display:none')
+        msgSucess.innerHTML = ''
     }
 
 }
